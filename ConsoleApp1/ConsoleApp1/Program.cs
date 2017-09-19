87cb6eb4b4f0adeab9c770b29230b4e26bea5995
Charles-M5 <31874901+Charles-M5@users.noreply.github.com>
2017-09-19 19:30:36 -0400

    blah
    blah

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName;
+            string middleInitial;
+            string lastName;
+            string fullName;
+            int age;
+            bool isCitizen;
+            bool canVote;
+            int heightFeet;
+            double heightInches;
+            double totalHeightCM;
+
+
+        }
+    }
+}
